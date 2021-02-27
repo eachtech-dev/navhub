a34f4a7e147157cef46a584eb11b2996debf193e
Alexey Yakovlev <alexeyyakovlev0359@gmail.com>
2021-02-27 18:03:46 +0300

    feat: add jest tests support

---
+++ src/hooks/useToggle/index.ts
@@ -0,0 +1,2 @@
+export { default } from './useToggle.hook';
+export * from './useToggle.hook';
---
+++ src/hooks/useToggle/useToggle.hook.ts
@@ -0,0 +1,15 @@
+import { useCallback, useState } from 'react';
+
+export type TUseToggle = (defaultValue?: boolean) => [boolean, () => void];
+
+export const useToggle: TUseToggle = (defaultValue) => {
+    const [value, setValue] = useState<boolean>(Boolean(defaultValue));
+
+    const toggle = useCallback(() => {
+        setValue((value) => !value);
+    }, [setValue]);
+
+    return [value, toggle];
+};
+
+export default useToggle;
