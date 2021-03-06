f52fcb90c2da1f14def7658547c0d313b4f1e26a
Alexey Yakovlev <alexeyyakovlev0359@gmail.com>
2021-03-07 01:00:30 +0300

    refactor: improve translations import

--- .storybook/loaders/translations.js
+++ .storybook/loaders/translations.js
@@ -1,10 +1,9 @@
 import { fetch } from '../../src/utils/fetch';
 
-import translationsJson from '../translations.json';
-
 export const translations = async () => {
     const translations =
-        (await translationsJson) || (await fetch('/translations'));
+        (await import('../translations.json')) ||
+        (await fetch('/translations'));
 
     return { translations };
 };
